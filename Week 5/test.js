a422d952f1770e5cdac7249e404ff05e475c07be
Chris <chris@chris-agnew.com>
2021-04-20 17:29:01 -0400

    Change: test .js

--- Week 5/test.js
+++ Week 5/test.js
@@ -83,17 +83,17 @@
 
 // console.log(countGoodTriplets(arr, a, b, c));
 
-const canPlaceFlowers = (flowerbed, n) => {
-  for (let i = 0; i < flowerbed.length && n !== 0; i++) {
-    if (
-      flowerbed[i] === 0 &&
-      flowerbed[i - 1] !== 1 &&
-      flowerbed[i + 1] !== 1
-    ) {
-      n--;
-    }
-  }
-  return n === 0;
-};
+// const canPlaceFlowers = (flowerbed, n) => {
+//   for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+//     if (
+//       flowerbed[i] === 0 &&
+//       flowerbed[i - 1] !== 1 &&
+//       flowerbed[i + 1] !== 1
+//     ) {
+//       n--;
+//     }
+//   }
+//   return n === 0;
+// };
 
-console.log(canPlaceFlowers([1, 0, 0, 0, 0, 0, 1], 2));
+// console.log(canPlaceFlowers([1, 0, 0, 0, 0, 0, 1], 2));
