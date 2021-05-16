5f256e63ca3c1b86d77d545e8d065f14677452ff
Chris <chris@chris-agnew.com>
2021-05-16 14:38:37 -0400

    Practice for Test

---
+++ Week 8/newBst.js
@@ -0,0 +1,45 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let node = new Node(value);
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+    let current = this.root;
+
+    while (current) {
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = node;
+          return this;
+        }
+        current = current.left;
+      }
+      if (value > current.value) {
+        if (!current.right) {
+          current.right = node;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+}
+
+let bst = new BST();
+bst.insert(57);
+bst.insert(43);
+bst.insert(65);
+console.log(bst);
